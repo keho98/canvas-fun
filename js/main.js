a6bde7200ab80e89bcfa9c0cf94720dd2b050539
Kevin Ho <keho98@gmail.com>
2013-01-13 16:44:59 -0800

    Finished mondrian

--- js/main.js
+++ js/main.js
@@ -1,20 +1,53 @@
-$((function(){
+/*
+ * Mondrian Generator
+ * Basic mondrian like generator using recursion
+ */
+
+$(function(){
 	var canvas = $('#main').get(0);
 	var speed = 30;
-	
+
+	function getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 	function init(){
-		setInterval(draw,50);
+		draw();
 	}
 
 	function draw(){
 		if (canvas.getContext) {
 		  var ctx = canvas.getContext("2d");
-
-		  ctx.fillStyle = "rgb(200,0,0)";
-		  ctx.fillRect (10, 10, 55, 50);
-
-		  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-		  ctx.fillRect (30, 30, 55, 50);
+		  drawMondrian(0,0,720,480, ctx, ["rgb(200,0,0)", "rgb(0,200,0)", "rgb(0,0,200)", "rgb(256,256,256)", "rgb(256,256,256)"], 4, 10);
 		}
 	}
+
+	function drawMondrian(x,y,w,h, context, colors, levels, strokeWidth){
+		context.beginPath();
+		context.rect(x,y,w,h);
+		context.fillStyle = colors[getRandomInt(0, colors.length - 1)];
+		context.fill();
+		context.strokeStyle = "rgb(0,0,0)";
+		context.lineWidth = strokeWidth;
+		context.stroke();
+		if(levels === 0) return;
+		else{
+			//Vertical Split
+			if(Math.random() < .5){
+				var randomX = getRandomInt(x, x+w);
+				drawMondrian(randomX,y,x+w - randomX,h, context, colors, levels-1);
+				drawMondrian(x,y,randomX - x,h, context, colors, levels-1);
+			}
+			//Horizontal Split
+			else{
+				var randomY = getRandomInt(y, y+h);
+				drawMondrian(x, randomY, w, y+h - randomY, context, colors, levels-1);
+				drawMondrian(x, y, w, randomY - y, context, colors, levels - 1);
+			}
+		}
+	}
+	$('.redraw').on('click', function(){
+		draw();
+	})
+	init();
 });
